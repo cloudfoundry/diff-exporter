27fee66eaa9a078a6c44057b9e319f0e4c508a0c
Garden Windows <garden-windows-eng@pivotal.io>
2019-08-05 14:54:46 -0400

    Revert "Roll back hcsshim to v0.8.3-mod"
    This reverts commit ecdb873f8c1ef88c4be3ebd7c997d18511e424e9.
    
    Turns out we can't go back to v0.8.3 because
    it has a race condition bug. See 9aec99

--- vendor/github.com/Microsoft/hcsshim/hnsendpoint.go
+++ vendor/github.com/Microsoft/hcsshim/hnsendpoint.go
@@ -6,13 +6,6 @@ import (
 
 // HNSEndpoint represents a network endpoint in HNS
 type HNSEndpoint = hns.HNSEndpoint
-type Resources = hns.Resources
-type Allocator = hns.Allocator
-
-const (
-	EndpointPortType = 2
-	NATPolicyType    = 4
-)
 
 // Namespace represents a Compartment.
 type Namespace = hns.Namespace
